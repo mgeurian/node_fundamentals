beecb3f449140a2a38a5960fb1cce71146c68b70
Matt Geurian <mgeurian@gmail.com>
2020-11-17 19:10:17 -0600

    updated for new eyes

--- app.js
+++ app.js
@@ -1,82 +1,126 @@
 var favBtn = document.getElementById("favBtn");
-var results = document.getElementById("results")
+var results = document.getElementById("results");
+var multifacts = document.getElementById("multifacts");
+
 
 favBtn.onclick = function() {
   results.innerHTML = '';
-
   var favNum = document.getElementById("favNum").value;
-
   const regex = /()\W/g;
 
+
+  // need error handling for inability to get multiple facts for multiple 'favorite' numbers
+  
   if (!favNum) {
       alert('enter your favorite number')
+
   } else if (favNum.match(regex)){
+    multiNumFact(favNum)
 
-    // *********** code for getting multiple facts here ************
+  } else if(!multifacts.checked){
+    singleFavNumFact(favNum);
 
-      favNum = favNum.replace(/(\s)/g, '');
-
-      let url = `http://numbersapi.com/${favNum}`
-      let ourPromise = axios.get(url);
-      ourPromise = axios.get(url);
-
-      ourPromise.then(data => handleResponse(data))
-
-      ourPromise.catch(err => console.log("REJECTED!", err))
-
-    // *******************************************************************
-    
   } else {
-
-    // ********** code for getting a favorite number fact here ***********
-
-    // ** numbers must be separated by commas **
-
-    let url = `http://numbersapi.com/${favNum}/?json`;
-    let ourPromise = axios.get(url);
-    
-    ourPromise.then(data => handleResponse(data))
-
-    ourPromise.catch(err => console.log("REJECTED!", err))
-
-    // *******************************************************************
+    multiFavNumFacts(favNum)
 
   }
 }
 
 
 
-function handleResponse(data){
-  let res = data.data;
+  // ********** get favorite number fact here ***********
+
+function singleFavNumFact(numData){
+
+  let url = `http://numbersapi.com/${numData}/?json`;
+  let ourPromise = axios.get(url);
+  
+  ourPromise.then(data => handleResponse(data))
+
+  ourPromise.catch(err => console.log("REJECTED!", err))
+}
+
+// *********** get multiple facts for different numbers here ************
+
+function multiNumFact(numData){
+  favNum = numData.replace(/(\s)/g, '');
+
+  let url = `http://numbersapi.com/${favNum}`
+  let ourPromise = axios.get(url);
+  ourPromise = axios.get(url);
+
+  ourPromise.then(data => handleResponse(data))
+
+  ourPromise.catch(err => console.log("REJECTED!", err))
+}
+
+// *********** get multiple facts for favorite number here
+
+function multiFavNumFacts(numData){
+
+  let fourFactsPromises = [];
+
+  for (let i = 1; i < 5; i++) {
+    fourFactsPromises.push(
+      axios.get(`http://numbersapi.com/${numData}`)
+    );
+  }
+
+  let fourFacts = [];
+
+  Promise.all(fourFactsPromises)
+    .then(factsArr => (
+      factsArr.forEach(fact => fourFacts.push(fact))
+    ))
+    .catch(err => console.log(err));
+
+  handleResponse(fourFacts)
+}
+
+// *********** return data from api to user ***********
+
+function handleResponse(factInfo){
+console.log(factInfo)
+  let res = factInfo.data;
 
   let facts = [];
 
-  if (res.text){
+  if(multifacts.checked){
+    console.log(factInfo)
+
+    factInfo.forEach(fact => console.log(fact.data))
+    // factInfo.forEach(fact => $("results").append(`<p>${fact.data}</p>`))
+
+
+  } else {
+  
+    if (res.text){
 
     let fact = `
       ${res.text}
     `;
 
-
     return results.append(fact)
 
-  } else {
-        
-    for(var key in res) {
+    } else {
 
-      // ***** neither of these seem to work using html within template literals
+      for(var key in res) {
 
-      // * option 1 *
-      let fact = `<p>${res[key]}.</p>`
-      facts.push(fact)
+        // ***** come back later and get this to work with jquery's append method for a cleaner look
 
-      // * option 2 *
+        // * option 1 *
+        // let fact = `<p>${res[key]}</p>`;
 
-      // facts.push(`<p>${res[key]}</p>`)
+        // results += fact;
 
-    }
+        // * option 2 *
+
+        facts.push(`${res[key]}`)
+
+      }
+
+      return results.innerHTML = facts;
     
-    return results.append(facts)
-    
+    }  
   }
 }
